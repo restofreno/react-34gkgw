cb4e4f058985376394cd5f1b1b07f0a2d0d13112
restofreno <restofreno@gmail.com>
2021-04-06 11:27:31 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      div.grid-container
-
+      <div class="grid-container">
+        <div class="grid-item" />
+        <div class="grid-item" />
+        <div class="grid-item" />
+        <div class="grid-item" />
+        <div className="grid-item" />
+        <div className="grid-item" />
+        <div className="grid-item" />
+        <div className="grid-item" />
+        <div className="grid-item" />
+        <div className="grid-item" />
+      </div>
     </div>
   );
 }
