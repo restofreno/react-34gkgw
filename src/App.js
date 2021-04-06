0765a6a70c0d421558cdfc435f93af1a45786b91
restofreno <restofreno@gmail.com>
2021-04-06 11:07:01 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      div.grid-container
+
     </div>
   );
 }
